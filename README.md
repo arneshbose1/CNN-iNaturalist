93a272337ea90443bc607475dbc983c428562736
Arnesh Bose <arneshbose1@gmail.com>
2021-07-31 23:40:28 +0530

    CNN code

---
+++ README.md
@@ -0,0 +1,36 @@
+# CS6910_Assignment2
+
+The description of the different files in the repository are as follows:
+
+Part A
+
+1. best_model - Contains the saved weights of the best model obtained after training.
+2. wandb - Contains the local files of the all runs performed on wandb.
+3. partA.ipynb - Final .ipynb for part A which can be executed cell by cell.
+4. sweep.yaml - Contains the hyperparameter values to run the sweep in wandb for Part A.
+5. train.py - Similar to the .ipynb file but in .py format
+
+The weights of the best model - https://drive.google.com/drive/folders/1OhekERA1Agk8viUKBFEwt1EzyACBi07K?usp=sharing
+
+Best results for Part A - Training Accuracy = 0.5949, Train Loss = 1.183, Validation Accuracy = 0.3984, Validation Loss = 1.998
+
+Part B
+
+1. CS6910_Assignment_2_PartB.ipynb - Final .ipynb for part B which can be executed cell by cell.
+2. sweep_part_B.yaml -  Contains the hyperparameter values to run the sweep in wandb for Part B.
+3. train_part_B.yaml - Similar to the .ipynb file but in .py format
+
+The weights of the best model - https://drive.google.com/drive/folders/1TGIzIxvRy0BjUWPc4WDkTo_TeV66X6gJ?usp=sharing
+
+Best results for Part B - Training Accuracy = 0.6908, Train Loss = 0.9443, Validation Accuracy = 0.6326, Validation Loss = 1.377
+
+
+Part C
+
+1. The PartC.ipynb file has the detailed that I have used to do real time object identification using Yolov3.
+2. The folder also has the pre trained weights that I have used.
+3. The source of the weights is a GitHub repository that I have mentioned in the report.
+
+
+
+Link to wandb report - https://wandb.ai/arnesh_neil/CS6910_Assignment_2/reports/CS6910-Assignment-2-Report--Vmlldzo1NjgyNTI
---
+++ partB/train_part_B.py
@@ -0,0 +1,125 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import  models, optimizers, layers, activations
+from tensorflow.keras.models import Model
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import InceptionV3, InceptionResNetV2, ResNet50, Xception
+
+import wandb
+from wandb.keras import WandbCallback
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+default_parameters = dict(
+    data_aug = True,
+    pre_trained_model = 'InceptionResNetV2',
+    batch_size = 32,
+    final_dense = 32,
+    final_dropout = 0.2,
+    k_freeze_percent = 0.6, 
+    batch_norm = True,
+    activation = "relu"
+    )
+
+
+run = wandb.init(config= default_parameters,project="CS6910_Assignment_2", entity="arnesh_neil")
+config = wandb.config
+
+image_size = (128,128)
+input_size = (128,128,3)
+batch_size = config.batch_size
+
+class_names = ['Amphibia', 'Animalia', 'Arachnida', 'Aves', 'Fungi',
+               'Insecta', 'Mammalia', 'Mollusca', 'Plantae', 'Reptilia']
+
+data_aug = config.data_aug
+
+if data_aug:
+    train_gen = ImageDataGenerator(rotation_range=45, width_shift_range=0.2, height_shift_range=0.2,
+                                 shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='reflect',
+                                   rescale=1./255, validation_split=0.1)
+else:
+    train_gen = ImageDataGenerator(rescale=1./255, validation_split=0.1)
+
+train_ds = train_gen.flow_from_directory(
+    directory='nature_12K/inaturalist_12K/train/',
+    target_size=image_size,
+    color_mode="rgb",
+    batch_size=batch_size,
+    class_mode="categorical",
+    shuffle=True,
+    seed=42,
+    subset="training")
+
+val_ds = train_gen.flow_from_directory(
+    directory='nature_12K/inaturalist_12K/train/',
+    target_size=image_size,
+    color_mode="rgb",
+    batch_size=batch_size,
+    class_mode="categorical",
+    shuffle=True,
+    seed=42,
+    subset="validation")
+
+
+def use_pretrained_model(pre_trained_model, batch_size, final_dense, final_dropout, k_freeze_percent, batch_norm, activation):
+    if pre_trained_model == 'InceptionV3':
+        model = InceptionV3(input_shape = input_size, include_top = False, weights = 'imagenet') # leaving out the last layer since we have only 10 classes
+        k = math.ceil(k_freeze_percent*311)
+    elif pre_trained_model == 'InceptionResNetV2':
+        model = InceptionResNetV2(input_shape = input_size, include_top = False, weights = 'imagenet')
+        k = math.ceil(k_freeze_percent*780)
+    elif pre_trained_model == 'ResNet50':
+        model = ResNet50(input_shape = input_size, include_top = False, weights = 'imagenet')
+        k = math.ceil(k_freeze_percent*175)
+    elif pre_trained_model == 'Xception':
+        model = Xception(input_shape = input_size, include_top = False, weights = 'imagenet')
+        k = math.ceil(k_freeze_percent*132)
+        
+    
+    i = 0
+    for layer in model.layers:
+        layer.trainable = False
+        i+=1
+        if(i==k):
+            break
+        
+    x = layers.Flatten()(model.output) # flattening the last layer to a single layer
+    
+    if activation == "relu":
+        x = layers.Dense(final_dense, layers.ReLU())(x)
+    elif activation == "leaky_relu":
+        x = layers.Dense(final_dense, layers.LeakyReLU(alpha=0.1))(x)
+
+    if batch_norm:
+        x = layers.BatchNormalization()(x)
+
+    x = layers.Dropout(final_dropout)(x) # adding a dropout
+    x = layers.Dense(10, tf.nn.softmax)(x) # final softmax function
+    
+    final_model = Model(model.input,x)
+    final_model.summary()
+    
+    return final_model
+
+pre_trained_model = config.pre_trained_model
+batch_size = config.batch_size
+final_dense = config.final_dense
+final_dropout = config.final_dropout
+k_freeze_percent = config.k_freeze_percent
+batch_norm = config.batch_norm
+activation = config.activation
+
+model = use_pretrained_model(pre_trained_model, batch_size, final_dense, final_dropout, k_freeze_percent, batch_norm, activation)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(train_ds, validation_data=val_ds, epochs=10, callbacks=[WandbCallback()])
